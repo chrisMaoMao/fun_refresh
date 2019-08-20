e3d4c1fe600c7b4a6221bbbde8f63453c72c85ec
蔡凯歌 <1289181391@qq.com>
2019-08-20 18:32:59 +0800

    苹果构建测试版

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,9 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/caikaige/Library/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/caikaige/Documents/FlutterPro/fun_refresh"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/caikaige/Documents/FlutterPro/fun_refresh/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/caikaige/Library/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=0.1.002"
-export "FLUTTER_BUILD_NUMBER=0.1.002"
+export "FLUTTER_BUILD_NAME=1.0.1"
+export "FLUTTER_BUILD_NUMBER=1.0.1"
